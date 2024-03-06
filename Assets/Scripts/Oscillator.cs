808bb03e5d23fa3da5d40d7dcfc3d485f44729d6
Devesh Ghodpage <deveshghodpage333@yahoo.in>
2024-03-06 23:59:55 +0530

    Resolved error in Oscillator.cs
    Resolved NaN error in Oscillator.cs by using Mathf.Epsilon function.

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
+    float movementFactor;
 
     [SerializeField] Vector3 movementVector;
-    float movementFactor;
     [SerializeField] float period = 2f;
-
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +19,10 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(period <= Mathf.Epsilon)
+        {
+            return;
+        }
         float cycles = Time.time / period; //continually growing over time
         const float tau = Mathf.PI * 2; //constant value of 6.283
 
